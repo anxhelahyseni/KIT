20e646a4c54e450e99f2184c37e049666212ca2a
Anxhela Hyseni <anxhela.hyseni1996@gmail.com>
2017-01-21 17:56:08 +0100

    Add files via upload

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -2,20 +2,28 @@
 
 @section('title', ' | Krijo Kurs Të Ri')
 
+@section('stylesheets')
+  {!! Html::style('css/parsley.css') !!}
+@endsection
+
 @section('content')
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <h1 style="text-align:center;">Krijo Kurs Të Ri</h1>
       <hr>
-      {!! Form::open(['route' => 'courses.store']) !!}
+      {!! Form::open(['route' => 'courses.store', 'data-parsley-validate'=>'']) !!}
           {{Form::label('title', 'Titulli:')}}
-          {{Form::text('title', null, array('class'=>'form-control', 'style'=>'margin-bottom:20px;'))}}
+          {{Form::text('title', null, array('class'=>'form-control', 'required'=>'', 'maxlength'=>'255'))}}
 
-          {{Form::label('body', 'Përshkrimi Kursit:')}}
-          {{Form::textarea('body', null, array('class'=>'form-control'))}}
+          {{Form::label('body', 'Përshkrimi Kursit:', array('style'=>'margin-top:20px;'))}}
+          {{Form::textarea('body', null, array('class'=>'form-control', 'required'=>''))}}
 
-          {{Form::submit('Krjo Kurs', array('class'=>'btn btn-success btn-lg btn-block', 'style'=>'margin-top:20px;'))}}
+          {{Form::submit('Krijo Kurs', array('class'=>'btn btn-success btn-lg btn-block', 'style'=>'margin-top:20px;'))}}
       {!! Form::close() !!}
     </div>
   </div>
+@endsection
+
+@section('scripts')
+  {!! Html::script('js/parsley.min.js') !!}
 @endsection
