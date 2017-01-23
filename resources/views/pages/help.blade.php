b44d935ee135b4547956f85eb927c7a01053df48
Anxhela Hyseni <anxhela.hyseni1996@gmail.com>
2017-01-23 20:50:29 +0100

    Add files via upload

---
+++ resources/views/pages/help.blade.php
@@ -0,0 +1,12 @@
+@extends('main')
+
+@section('title', ' | Ndihmë')
+
+@section('content')
+  <div class="row">
+    <div class="col-md-12">
+      <h1>Ndihmë</h1>
+      <p>...</p>
+    </div>
+  </div>
+@endsection
